28dceffec53216d94fa83e8507260a67540f70a6
tamauri <root@localhost>
2017-01-20 21:24:21 -0500

    updated passwd_file.py

--- passwd_file.py
+++ passwd_file.py
@@ -5,6 +5,7 @@ def cleanPasswd(arg1, arg2):
 	This is a docstring blah blah blah
 	''' 
 
+
 filename = '/etc/passwd'
 passwd = open(filename)
 
@@ -12,6 +13,8 @@ fields = {
 	0: "username", 1: "password", 2: "user id", 3: "group id", 
 	4: "comments", 5: "homedir", 6: "shell"
 }
+
+# use modern interpolation - reminder
 
 for line in passwd:
 	parts = line.strip().split(':')
