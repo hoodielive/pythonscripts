df20d37d6afaa7310b4d62cc659755520b6a54d4
hoodielive <hoodielive@gmail.com>
2017-05-21 13:11:36 -0500

    Making it a function

--- subprocesses-calls.py
+++ subprocesses-calls.py
@@ -2,6 +2,7 @@
 
 import subprocess 
 
+def uname_func(): 
 # Command 1 
 uname = "uname"
 uname_arg = "-a" 
@@ -9,6 +10,8 @@ print("Gathering System Information with {} command:\n".format(uname))
 subprocess.call([uname, uname_arg]) 
 
 
+
+def diskspace_func(): 
 # Command 2 
 
 diskspace = "df" 
@@ -17,3 +20,8 @@ print("Gathering Disk Information with {} command:\n".format(diskspace))
 subprocess.call([diskspace, diskspace_arg]) 
 
 
+def main():
+    uname_func()
+    diskspace_func() 
+
+main() 
