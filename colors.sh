b6d81f214fe26ded99371bf6d581786d0f7006a7
hoodielive <hoodielive@gmail.com>
2017-05-21 18:50:38 -0500

    Colors in bash

---
+++ colors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
+echo -e "$COL_RED This is the power of prophecy $COL_RESET"
+echo -e "$COL_BLUE This is the power of prophecy $COL_RESET"
+echo -e "$COL_MAGENTA This is the power of prophecy $COL_RESET"
+
+
