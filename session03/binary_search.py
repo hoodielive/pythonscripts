89f91e35b6cfe012cc7a08c015047a25747f51e0
hoodielive <hoodielive@gmail.com>
2018-03-10 09:58:49 -0500

    This is a refactor

--- session03/binary_search.py
+++ session03/binary_search.py
@@ -1,17 +1,20 @@
+my_list = [1,3,5,7,9]
 low = 0
 high = len(my_list) - 1
 mid = (low + high) / 2 
+guess = 15
+item = 20030
 
 if guess < item: 
     low = mid + 1 
 
-def binary_search(list, item): 
+def binary_search(my_list, item): 
     low = 0 
-    high = len(list) - 1
+    high = len(my_list) - 1
     
     while low <= high: 
         mid = (low + high) 
-        guess = list[mid]
+        guess = my_list[mid]
         if guess == item: 
             high = mid - 1
         if guess > item: 
@@ -19,7 +22,5 @@ def binary_search(list, item):
         else: 
             low = mid + 1
     return None 
-
-my_list = [1,3,5,7,9]
 
 print(binary_search(my_list, 3))
