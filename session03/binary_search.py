0197f2e3a3a032a5cc888e6cf37e78fedd755fa3
hoodielive <hoodielive@gmail.com>
2018-03-10 09:50:43 -0500

    binary_search algorithm

---
+++ session03/binary_search.py
@@ -0,0 +1,25 @@
+low = 0
+high = len(my_list) - 1
+mid = (low + high) / 2 
+
+if guess < item: 
+    low = mid + 1 
+
+def binary_search(list, item): 
+    low = 0 
+    high = len(list) - 1
+    
+    while low <= high: 
+        mid = (low + high) 
+        guess = list[mid]
+        if guess == item: 
+            high = mid - 1
+        if guess > item: 
+            high  = mid - 1
+        else: 
+            low = mid + 1
+    return None 
+
+my_list = [1,3,5,7,9]
+
+print(binary_search(my_list, 3))
