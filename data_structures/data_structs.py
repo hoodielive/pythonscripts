135b5912507049f37e30c9988f279027f32b662d
hoodielive <hoodielive@gmail.com>
2018-03-27 19:50:29 -0400

    python data structures

---
+++ data_structures/data_structs.py
@@ -0,0 +1,23 @@
+# lists, sets, strings, tuples, dictionaries, deque, heaps 
+
+
+# dictionaires
+dict = {
+        'name' : 'esu', 
+        'age' : 32394390438,
+        'city' : 'Nigeria' 
+} 
+
+print(dict['name']) 
+
+for key, value in dict.items(): 
+    print(key, value)
+
+
+# lists, they are indexed  
+list = [1,2,3,4] 
+print(list) 
+print(list[2]) 
+
+
+
