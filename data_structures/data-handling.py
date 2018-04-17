78c447549058b15132e1fb9ab870a3b790293e5f
hoodielive <hoodielive@gmail.com>
2018-04-16 20:55:13 -0400

    Data-handling is a skill to master

---
+++ data_structures/data-handling.py
@@ -0,0 +1,8 @@
+empty_list = [] 
+fardon = ['earth', 'sky', 'trees', 'fields', 'water', 'spirit', 'aethyrs'] 
+
+for items in fardon:
+    empty_list.append(items)
+    print(empty_list, end=' ') 
+
+
