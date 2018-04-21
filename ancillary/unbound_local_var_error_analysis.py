84c4d48e146173f5ec4b5d7879c51d59365a1339
hoodielive <hoodielive@gmail.com>
2018-04-21 18:44:27 -0400

    Unbound Local Var expanding

--- ancillary/unbound_local_var_error_analysis.py
+++ ancillary/unbound_local_var_error_analysis.py
@@ -1,3 +1,4 @@
+# Code Objects -> https://docs.python.org/3/reference/datamodel.html?highlight=co_nlocals#the-standard-type-hierarchy
 def create_unbound_local_var_error():
     print('In this test == ', x) 
     x = 3
@@ -14,3 +15,6 @@ create_unbound_local_var_error.__code__.co_varnames
 import dis
 
 dis.dis(create_unbound_local_var_error.co_code) 
+
+dis.dis(codeop.compile_command('def t1(): a = b; b = 7')) 
+dis.dis(codeop.compile_command('def t2(): b = 7, a = b')) 
