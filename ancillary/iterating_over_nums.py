24ddb5eaa673c85b71008c52b0354509f73be212
hoodielive <hoodielive@gmail.com>
2018-05-03 07:31:25 -0400

    iterating over numbers

---
+++ ancillary/iterating_over_nums.py
@@ -0,0 +1,7 @@
+def normalize(numbers):
+    total = sum(numbers) 
+    result = []
+    for value in numbers:
+        percent = 100 * value / total 
+        result.append(percent)
+    return result
