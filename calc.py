c5d4ef4ce1569db6277c69ba2f4d2917bc2c1f87
hoodielive <hoodielive@gmail.com>
2018-07-14 22:24:32 -0400

    session with joel calc

---
+++ calc.py
@@ -0,0 +1,10 @@
+products = ["toy1", "toy2", "toy3", "toy4"] 
+
+for index in range(len(products)):
+    question = int(input("Yo what is your number?"))
+    #question = sum(int(input("string" + index + "?"))) 
+    print(question + index) 
+  #  if question[index] < len(products):
+  #      print("keep going")
+  #  else:
+  #      break
