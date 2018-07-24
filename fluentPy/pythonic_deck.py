0466b4f89910f9aa42a8022f50401a73494cc844
hoodielive <hoodielive@gmail.com>
2018-07-24 17:51:26 -0400

    Pythonic Deck

--- fluentPy/pythonic_deck.py
+++ fluentPy/pythonic_deck.py
@@ -1,7 +1,6 @@
+#!/usr/bin/env python3
 import collections 
-
 Card = collections.namedtuple('Card', ['rank', 'suit']) 
-
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA') 
     suits = 'spades diamonds clubs hearts'.split() 
