30f22c09fcfcfc7f64b3b960c9caccb9490a170b
hoodielive <hoodielive@gmail.com>
2018-07-25 10:34:14 -0400

    created idx_to_token as first vector

---
+++ deepLearning/vector.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+
+import numpy
+
+idx_to_token = list(set(tokens))
+
+token_to_idx = { token: idx for idx, token in enumerate(idx_to_token) } 
+
+one_hot = lambda token: [ 1 if i == token_to_idx[token] else 0 for i in range(len(idx_to_token)) ] 
+
+encoded = np.asarray([one_hot(token) for token in tokens]) 
