27e29cbaa983d3556c08832a3ad9243472a18076
hoodielive <hoodielive@gmail.com>
2018-07-25 16:46:03 -0400

    started and didn't finish numpy

---
+++ numpy/numpy.py
@@ -0,0 +1,8 @@
+import numpy 
+
+nums = [1, 4.5, 6.25, 8, 10, 15]
+
+# nums + 1 # will generate an error because you can only concatenate list (not 'int') to list 
+
+a = numpy.array(nums) 
+print(a) 
