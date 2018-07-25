20c8d26080573aed547a4e3da286aff0de4070e5
hoodielive <hoodielive@gmail.com>
2018-07-25 10:49:59 -0400

    Vectoring... <lol>

--- deepLearning/vector.py
+++ deepLearning/vector.py
@@ -8,4 +8,4 @@ token_to_idx = { token: idx for idx, token in enumerate(idx_to_token) }
 
 one_hot = lambda token: [ 1 if i == token_to_idx[token] else 0 for i in range(len(idx_to_token)) ] 
 
-encoded = np.asarray([one_hot(token) for token in tokens]) 
+print(encoded = np.asarray([one_hot(token) for token in tokens])) 
